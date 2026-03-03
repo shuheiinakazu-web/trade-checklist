88227fddad532f26f88d72789a329c9c291fb41f
shuheiinakazu-web <shuhei.inakazu@gmail.com>
2026-03-03 23:36:38 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'trade-precheck-cache-v1';
+const OFFLINE_URLS = [
+  './',
+  './index.html',
+  './manifest.webmanifest'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(OFFLINE_URLS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
